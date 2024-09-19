05a7f09da95f3ba2099f364954d589df1575c7d7
Jack Carney <jackcarney09@outlook.com>
2024-09-19 11:05:07 +0100

    making it into a function

--- Activity 3.py
+++ Activity 3.py
@@ -1,20 +1,24 @@
-total = 0 
+def adding_numbers():
 
-num1 =  int(input("Enter a number:"))
-num2 =  int(input("Enter a number:"))
 
-total = num1 + num2
-    
-flag = True 
-while flag: 
-    user_input = input("Enter a do you want to add another numer?:").upper()
-    
-    if user_input == "YES"or user_input == "Y":
-        choice = int(input("Enter a number:"))
-        total += choice
-    elif user_input == "NO" or user_input == "N":
-        print("The total is:", total)
-        break
-    else :
-        print ("Invalid input")
-        continue
+    total = 0 
+
+    num1 =  int(input("Enter a number:"))
+    num2 =  int(input("Enter a number:"))
+
+    total = num1 + num2
+        
+    flag = True 
+    while flag: 
+        user_input = input("Enter a do you want to add another numer?:").upper()
+        
+        if user_input == "YES"or user_input == "Y":
+            choice = int(input("Enter a number:"))
+            total += choice
+        elif user_input == "NO" or user_input == "N":
+            print("The total is:", total)
+            break
+        else :
+            print ("Invalid input")
+            continue
+adding_numbers
