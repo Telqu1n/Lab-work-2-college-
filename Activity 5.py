48117713b51d56403fb55771804a1b07d6fca8c6
Jack Carney <jackcarney09@outlook.com>
2024-09-19 11:46:35 +0100

    Completitng my activity

--- Activity 5.py
+++ Activity 5.py
@@ -1,17 +1,26 @@
-compnum = 50
+def number_counting_game():
 
-num1 =  int(input("Enter a number:"))
-flag = True 
-while flag:
-    if num1 < compnum:
-        print("The number your entered is lower than the number")
-        num1 =  int(input("Enter a number:"))
-    elif num1 > compnum:
-        print("The number your entered is higher than the number")
-        num1 =  int(input("Enter a number:"))
-    elif num1 == compnum:
-        print("The number your entered is equal to the number")
-        break
-    else:
-        print("Invalid input")
-        continue
+
+    compnum = 50
+    count = 0
+
+    num1 = int(input("Enter a number:"))
+    flag = True
+    while flag:
+        count += 1  # Increment count for each input
+        if num1 < compnum:
+            print("The number you entered is lower than the number")
+            num1 = int(input("Enter a number:"))
+        elif num1 > compnum:
+            print("The number you entered is higher than the number")
+            num1 = int(input("Enter a number:"))
+        elif num1 == compnum:
+            print("The number you entered is equal to the number")
+            break
+        else:
+            print("Invalid input")
+            continue
+
+    print("The number is:", compnum, "You guessed", count, "times")
+    
+number_counting_game9()
