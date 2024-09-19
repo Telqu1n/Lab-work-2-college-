7161a3d9c26ddc66736270327660295bcbaf3175
Jack Carney <jackcarney09@outlook.com>
2024-09-19 10:17:52 +0100

    Re indenting to code

--- Activity 2.py
+++ Activity 2.py
@@ -1,15 +1,13 @@
 def enter_number_until_its_over_5():
-
-
-user_input = int(input("Enter a number:"))
-
-while user_input < 5:
     user_input = int(input("Enter a number:"))
-    if user_input > 5:
-        break
+
+    while user_input < 5:
+        user_input = int(input("Enter a number:"))
+        if user_input > 5:
+            break
+        else:
+            continue
     else:
-        continue
-else:
-    print("The number entered is 5")
+        print("The number entered is 5")
     
 enter_number_until_its_over_5()
