8c76dbbb84fefe88ee172462b1bde782a2ed6001
Jack Carney <jackcarney09@outlook.com>
2024-09-19 09:57:33 +0100

    Commit my college work

---
+++ Activity 1.py
@@ -0,0 +1,11 @@
+
+def adding_to_50():
+    total = 0 
+
+    while total <= 50:
+        num = int(input("Enter a number:"))
+        total += num 
+    else:
+        print("Loop stopped, total is greater than 50")
+    
+adding_to_50()
