d19659704a4f32a620fc1e8b8e5dca9b8c4dc94a
Jack Carney <jackcarney09@outlook.com>
2024-09-19 10:10:47 +0100

    Completeing activity 2 for college

---
+++ Activity 2.py
@@ -0,0 +1,15 @@
+def enter_number_until_its_over_5():
+
+
+user_input = int(input("Enter a number:"))
+
+while user_input < 5:
+    user_input = int(input("Enter a number:"))
+    if user_input > 5:
+        break
+    else:
+        continue
+else:
+    print("The number entered is 5")
+    
+enter_number_until_its_over_5()
